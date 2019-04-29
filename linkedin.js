56a7250623c8b705fdf1811c8fee30bb883d3b19
Dane Lipscombe <dane@lipscombe.com.au>
2019-04-29 12:49:04 +1000

    Improve linkedin export

--- linkedin.js
+++ linkedin.js
@@ -1,6 +1,7 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const path = require("path");
+const YAML = require('yaml');
 
 let importPath = "linkedin-export";
 
@@ -18,10 +19,36 @@ function parseCSV(filename) {
 
 async function run() {
   const positions = await parseCSV("Positions.csv");
+  const profile = (await parseCSV("Profile.csv"))[0];
+  const education = await parseCSV("Education.csv");
+  const experience = positions.map(p => ({
+    dates: `${p["Started On"]} - ${p["Finished On"] || "Current"}`,
+    employer: p["Company Name"],
+    title: p["Title"],
+    city: p["Location"],
+    description: p["Description"].split("● ").filter(x => x)
+  }));
+
+  const certifications = education.map(e => ({
+    cert: e["Degree Name"],
+    description: `${e["Start Date"]} - ${e["End Date"]} at ${e["School Name"]}`
+  }));
+
   const result = {
-    positions
+    mainfont: "Helvetica",
+    firstname: profile["First Name"],
+    lastname: profile["Last Name"],
+    email: "dane@lipscombe.com.au",
+    linkedin: "danelipscombe",
+    github: "dlip",
+    twitter: "danelips",
+    homepage: "http://lipscombe.com.au/",
+    summary: profile["Summary"],
+    experience,
+    certifications
   };
-  console.log(JSON.stringify(result));
+  console.log(YAML.stringify(result));
+  fs.writeFileSync("resume.yml", "---\n" + YAML.stringify(result) + "\n...")
 }
 run();
 
